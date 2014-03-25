d84465c517a6a60dc5f6846e036142b1f7b8cdcf
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-03-25 08:16:31 +0800

    Changes Clips

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -19,11 +19,7 @@ $(function(){
             player.play();
         });
 
-        player.addEvent("loadeddata", function(){
-            player.play();
-        });
-
-        player.currentTime(moveTimeInSec);
+        //player.currentTime(moveTimeInSec);
     });
 
     socket.on('pause', function(){
--- routes/videos.js
+++ routes/videos.js
@@ -1,11 +1,12 @@
 var sequence = require("../sequence/sequence"),
-    _ = require("underscore")
+    _ = require("underscore"),
+    config = require("../config/config.js")
 
 exports.list = function(req, res) {
     //move this to a class
     var id = req.params.id,
         video = "vid" + id + ".mp4",
-        url = "http://192.168.1.3:3003/media/" + video,
+        url = "http://" + config.ip + ":3003/media/" + video,
         videoSequence = _.find(sequence, function(seq){ return seq.id == id})
 
     res.render('show', {
