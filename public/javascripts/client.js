68706afffba5bbe99d79d1bd536531e30f546cc9
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-03-25 08:07:45 +0800

    Changes to Mini Videos

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -8,8 +8,20 @@ $(function(){
 
     socket.on('move', function(data){
         //move functions to scene model in backbone
-        var moveTime = window.transitions[data.scene],
-            moveTimeInSec = moveTime / 1000;
+        //var moveTime = window.transitions[data.scene],
+            //moveTimeInSec = moveTime / 1000;
+
+        var src = window.transitions[data.scene];
+
+        player.src({ type: "video/mp4",  src: src });
+        player.load();
+        player.ready(function(){
+            player.play();
+        });
+
+        player.addEvent("loadeddata", function(){
+            player.play();
+        });
 
         player.currentTime(moveTimeInSec);
     });
--- routes/control.js
+++ routes/control.js
@@ -1,10 +1,11 @@
 var sequence = require("../sequence/sequence"),
-    _ = require("underscore")
+    _ = require("underscore"),
+    config = require("../config/config.js")
 
 exports.list = function(req, res) {
     var id = "main",
         video = id+".mp4",
-        url = "http://192.168.1.3:3003/media/" + video,
+        url = "http://" + config.ip + ":3003/media/" + video,
         videoSequence = _.find(sequence, function(seq){ return seq.id == id})
 
     res.render('control', {
